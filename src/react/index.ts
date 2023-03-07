ad585c0e17c83a3991528bbe0fd1948f2a06cdb5
Gordon <gburgett@watermark.org>
2023-03-07 13:38:12 -0600

    Forgot to export these

--- src/react/index.ts
+++ src/react/index.ts
@@ -3,3 +3,5 @@ export {LocalSchemaProvider, useDataSource} from './context'
 
 export {useFindEntry} from './hooks/useFindEntry'
 export {useFindAsset} from './hooks/useFindAsset'
+export {useQueryEntries} from './hooks/useQueryEntries'
+export {useQueryAssets} from './hooks/useQueryAssets'
